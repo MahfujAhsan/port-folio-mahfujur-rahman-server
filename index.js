70081589cc4b919b7324df220e26673c8e91843b
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-06-25 03:46:16 +0600

    updated

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j5g1e.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri)
-
 async function run() {
     try {
         await client.connect();
